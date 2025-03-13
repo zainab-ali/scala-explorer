ce52f646e5c18425dcab90ac329a821ef735d4f7
Anton Sviridov <keynmol@gmail.com>
2025-03-13 15:17:16 +0000

    wip

--- index.scala
+++ index.scala
@@ -2,22 +2,44 @@ import scalajs.js
 import org.scalajs.dom
 import com.raquo.laminar.api.L.*
 import com.raquo.laminar.nodes.ReactiveHtmlElement
+import com.raquo.airstream.web.WebStorageVar
 
 import scala.meta.*
-class TreeView(t: Tree):
-  val openNodes = Var(Set.empty[Int])
+import scala.util.Try
+class TreeView(t: Tree, openNodes: Var[Set[Int]]):
   val toggle = openNodes.updater[Int]: (cur, next) =>
     if cur(next) then cur - next else cur + next
+  def isToggled(id: Int) = openNodes.signal.map(_.contains(id))
+
+  lazy val root = div(
+    cls := "bg-gray-100 p-4 rounded-lg shadow-md",
+    code(encode(t))
+  )
+  lazy val (direct, reverse) = index(t)
 
   private def encode(t: Tree): Element =
     val id = reverse(t)
-    ul(
-      a(
-        href := "#",
-        onClick.preventDefault.mapToStrict(id) --> toggle,
-        t.productPrefix
+    span(
+      span(
+        a(
+          cls := "text-blue-700 text-sm",
+          href := "#",
+          child.text <-- isToggled(id).map: b =>
+            val moniker =
+              if t.children.isEmpty then "  " else if b then "- " else "+ "
+            moniker + t.productPrefix
+          ,
+          onClick.preventDefault.mapToStrict(id) --> toggle,
+          t.productPrefix
+        ),
+        " ",
+        i(
+          cls := "text-amber-700 text-xs font-mono",
+          s"[${t.pos.start};${t.pos.end}]"
+        )
       ),
-      div(
+      ul(
+        cls := "list-inside list-none ml-4",
         t.children.map(child => li(encode(child))),
         display <-- openNodes.signal
           .map(_.contains(id))
@@ -25,8 +47,6 @@ class TreeView(t: Tree):
       )
     )
   end encode
-  lazy val root = encode(t)
-  lazy val (direct, reverse) = index(t)
 
   private def index(t: Tree): (Map[Int, Tree], Map[Tree, Int]) =
     def go(
@@ -54,10 +74,20 @@ class TreeView(t: Tree):
 end TreeView
 
 def parse(s: String) =
+  val openNodes = WebStorageVar
+    .localStorage(key = "openNodes", syncOwner = None)
+    .withCodec[Set[Int]](
+      _.mkString(","),
+      a =>
+        Try(
+          a.split(',').flatMap(_.toIntOption).toSet
+        ),
+      Try(Set.empty)
+    )
+
   scala.meta.dialects.Scala3.apply(s).parse[Stat] match
     case x: Parsed.Success[Stat] =>
-      TreeView(x.tree).root
-    // index(x.tree)._1.toSeq.sortBy(_._1).mkString("\n\n").toString
+      TreeView(x.tree, openNodes).root
     case e: Parsed.Error =>
       p(s"ERROR: ${e.toString}")
 end parse
@@ -65,7 +95,20 @@ end parse
 @main def hello =
   val tg = Var(CODE)
   val parsed = tg.signal.map(parse)
-  val app = div(codeMirrorTextArea(tg), p(code(pre(child <-- parsed))))
+  val app =
+    div(
+      cls := "content mx-auto w-10/12 bg-white/70 p-6 rounded-xl flex flex-col gap-4 h-full",
+      h1("Scalameta AST explorer", cls := "text-4xl font-bold"),
+      p(
+        "This small webapp allows you to explore the AST of Scala code",
+        cls := "text-sm"
+      ),
+      div(
+        cls := "flex md:flex-col sm:flex-col lg:flex-row justify-baseline 2xl:flex-row gap-4 w-full",
+        div(cls := "w-6/12 h-full", codeMirrorTextArea(tg)),
+        div(cls := "w-6/12 h-full", p(code(pre(child <-- parsed))))
+      )
+    )
 
   renderOnDomContentLoaded(dom.document.getElementById("app"), app)
 end hello
